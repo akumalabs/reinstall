64d8aa51a39b1b2b06f3f883b551c094c4813879
Akuma Labs <admin@akumavm.com>
2026-07-11 22:50:17 +0700

    Add auto reinstall script for various OS options

---
+++ kernel.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+
+#if [[ $EUID -ne 0 ]]; then
+#    clear
+#   echo "Error: This script must be run as root!" 1>&2
+#    exit 1
+#fi
+
+if [ -f "/usr/bin/yum" ] && [ -d "/etc/yum.repos.d" ]; then
+    yum update -y
+    yum install -y wget curl
+    
+elif [ -f "/usr/bin/apt-get" ] && [ -f "/usr/bin/dpkg" ]; then
+    apt-get update --fix-missing
+    apt install -y curl wget
+    
+fi
+
+function CopyRight() {
+  clear
+  echo "########################################################"
+  echo "#                                                      #"
+  echo "#               Auto Reinstall Script                  #"
+  echo "#                                                      #"
+  echo "#               Author: AKUMAVM                        #"
+  echo "#               Last Modified: 11-11-2025              #"
+  echo "#                                                      #"
+  echo "#               Inspired by bin456789                  #"
+  echo "#                                                      #"
+  echo "########################################################"
+  echo -e "\n"
+}
+
+function Start() {
+  CopyRight
+  
+curl -O https://raw.githubusercontent.com/akumalabs/launch/main/reinstall.sh || wget -O reinstall.sh $_
+
+
+  echo -e "\nPlease select an OS:"
+  echo "  1) Latest Debian"
+  echo "  2) Latest Ubuntu"
+  echo "  3) Latest Almalinux"
+  echo "  4) Latest Alpine Linux"
+  echo "  5) Latest CentOS"
+  echo "  6) Kali Linux"
+  echo "  7) Windows 10 Pro"
+  echo "  8) Windows 11 Pro"
+  echo "  9) Windows Server 2012 R2 DC"
+  echo "  10) Windows Server 2016 DC"
+  echo "  11) Windows Server 2019 DC"
+  echo "  12) Windows Server 2022 DC"
+  echo "  13) Windows Server 2025 DC"
+# echo "  14) Windows 10 Pro Lite"
+# echo "  15) Windows 11 Pro Lite"
+  echo "  16) Netboot"
+  echo "  99) Custom DD image"
+  echo "  0) Exit"
+  echo -ne "\nYour option: "
+  read N
+  case $N in
+    1) bash reinstall.sh debian --password 123@@@ ;;
+    2) bash reinstall.sh ubuntu --password 123@@@ ;;
+    3) bash reinstall.sh alma --password 123@@@ ;;
+    4) bash reinstall.sh alpine --password 123@@@ ;;
+    5) bash reinstall.sh centos --password 123@@@ ;;
+    6) bash reinstall.sh kali --password 123@@@ ;;
+    7) bash reinstall.sh windows --image-name='Windows 10 Pro' --iso='https://fafda.to/d/fuxscqu93mnn?v=Vp6_aNhhYS79d6Q2fRUQOaZ2wJct9EsnrCVq8-GHjGrgS_TRciyd4VFeKYHBGOZp6xO42x8i24hJMAaLHJrTUeGrryy8jOn20HSvsc2eKb_jNhnIumZidL5VuQVky5GEXM5VWvU7X_5Wn2_Iu4Nk6oigtevIUwBAhMEvfq7EchuUy2b_mO9Ry0cK9xc7dlys-PIUHmjsQjwG5MGoVxO8Ip12ASnT02V0oCb0hotUsQR4wtk4wGe3h5mwsvV2r5J4Jg82U_kGnFqmQzxJGog35Ty_opN9mFUIYNzRkVw78dPM2OPoamqlRp1oqgFMut4e8VC8sYz--cOTr0RNUkY' --password Akuma12345 --allow-ping ;;
+    8) bash reinstall.sh windows --image-name='Windows 11 Pro' --iso='https://dl.zerofs.link/dl/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJidWNrZXQiOiJhc3NldHMtYW1zIiwia2V5Ijoib05xNVlUcml4ekJlZGtoRzJxZXl2cS80M2I3YzZkMDRmY2Q0ZDNhOGJjZjQ1NTA3MTlhOTI4ZiIsImZpbGVuYW1lIjoiZW4tdXNfd2luZG93c18xMV9jb25zdW1lcl9lZGl0aW9uc192ZXJzaW9uXzI1aDJfdXBkYXRlZF9tYXJjaF8yMDI2X3g2NF9kdmRfYTFjZjZjMzYuaXNvIiwiYnVja2V0X2NvZGUiOiJldSIsImVuZHBvaW50IjoiczMuZXUtY2VudHJhbC0wMDMuYmFja2JsYXplYjIuY29tIiwiZXhwIjoxNzc0OTgxNjYzLCJrZXlfYjY0IjoicURpemVNSENxQllDUEdPZDN3NFZXallIbGhVU3dXZ1M3YXkrb1dISWJKVT0iLCJrZXlfbWQ1IjoiNzg4STdydzl6ZTQwQnNHKzZCYzZGZz09In0.MOY-rYGRk9HxowAFD3DuDXl9XesgMai4yYOgsNv5h4Q' --password Akuma12345 --allow-ping ;;
+    9) bash reinstall.sh windows --image-name='Windows Server 2012 R2 SERVERDATACENTER' --iso='https://go.microsoft.com/fwlink/p/?LinkID=2195443' --password Akuma12345 --allow-ping ;;
+    10) bash reinstall.sh windows --image-name='Windows Server 2016 SERVERDATACENTER' --iso='https://go.microsoft.com/fwlink/p/?LinkID=2195174' --password Akuma12345 --allow-ping ;;
+    11) bash reinstall.sh windows --image-name='Windows Server 2019 SERVERDATACENTER' --iso='https://go.microsoft.com/fwlink/p/?LinkID=2195167' --password Akuma12345 --allow-ping ;;
+    12) bash reinstall.sh windows --image-name='Windows Server 2022 SERVERDATACENTER' --iso='https://go.microsoft.com/fwlink/p/?LinkID=2195280' --password Akuma12345 --allow-ping ;;
+    13) bash reinstall.sh windows --image-name='Windows Server 2025 SERVERDATACENTER' --iso='https://go.microsoft.com/fwlink/p/?LinkID=2293312' --password Akuma12345 --allow-ping ;;
+#   14) bash reinstall.sh windows --image-name='Windows 10 Pro' --iso='https://iso.akumavm.com/tiny10.iso' --password Akuma12345 --allow-ping ;;
+#   15) bash reinstall.sh windows --image-name='Windows 11 Pro' --iso='https://iso.akumavm.com/tiny11.iso' --password Akuma12345 --allow-ping ;;
+    16) bash reinstall.sh netboot.xyz ;;
+    99)
+      echo -e "\n"
+      read -r -p "Custom DD image URL: " imgURL
+      echo -e "\n"
+      read -r -p "Are you sure start reinstall? [y/N]: " input
+      case $input in
+        [yY][eE][sS]|[yY]) bash reinstall.sh dd --img $imgURL ;;
+        *) clear; echo "Canceled by user!"; exit 1;;
+      esac
+      ;;
+    0) exit 0;;
+    *) echo "Wrong input!"; exit 1;;
+esac
+}
+Start
